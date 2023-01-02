aa8d0cf7adb5b841a877d44f9059480910eef924
Isaías Subero <contactosubero@gmail.com>
2023-01-02 23:04:21 +0100

    Ingredients API endpoints.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -79,3 +79,12 @@ class ModelTests(TestCase):
             name='Sample tag name',
         )
         self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        """Test creating an ingredient is successful."""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Sample ingredient name',
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
